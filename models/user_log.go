eb07bddf2e2171e019d03e8e5de8e954374ae0f0
lixuancn <lixuan868686@163.com>
2015-12-15 14:55:47 +0800

    用户日志Model

---
+++ models/user_log.go
@@ -0,0 +1,51 @@
+package models
+import (
+	"time"
+	"github.com/astaxie/beego/orm"
+)
+
+type UserLog struct {
+	Id int64
+	User int64
+	Ip string
+	Ua string
+	Location string
+	Action int
+	CreateTime time.Time
+}
+func init(){
+	orm.RegisterModel(new(UserLog))
+}
+func (this *UserLog)TableName(){
+	return "user_log"
+}
+func (this *UserLog)AddUserLog(user int64, ip, ua, location string, action int)(int64, error){
+	o := orm.NewOrm()
+	o.Using("default")
+	userLog := new(UserLog)
+	userLog.User = user
+	userLog.Ip = ip
+	userLog.Ua = ua
+	userLog.Location = location
+	userLog.Action = action
+	return o.Insert(userLog)
+}
+func (this *UserLog)GetUserLogByIp(ip string)(UserLog, error){
+	o := orm.NewOrm()
+	o.Using("default")
+	userLog := UserLog{Ip:ip}
+	err := o.Read(&userLog, "ip")
+	return userLog, err
+}
+func (this *UserLog)IsVaildLocation(data map[string]interface{})bool{
+	o := orm.NewOrm()
+	o.Using("default")
+	cityName := data["city_name"].(string)
+	countryName := data["country_name"].(string)
+	regionName := data["region_name"].(string)
+	if len(cityName) == 0 && len(countryName) == 0 && len(regionName) == 0{
+		return false
+	}else{
+		return true
+	}
+}
