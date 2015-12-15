e64b175bb23d72a5a43059ab7c4faf39207743fe
lixuancn <lixuan868686@163.com>
2015-12-15 13:14:59 +0800

    添加tagModel

---
+++ models/tag.go
@@ -0,0 +1,25 @@
+package models
+import (
+	"time"
+	"github.com/astaxie/beego/orm"
+)
+
+type Tags struct {
+	Id int
+	Name string
+	Time time.Time
+}
+func (this *Tags)TableName()string{
+	return "tags"
+}
+func init(){
+	orm.RegisterModel(new(Tags))
+}
+
+func NewTag(tagName string)(int64, error){
+	o := orm.NewOrm()
+	o.Using("default")
+	tag := new(Tags)
+	tag.Name = tagName
+	return o.Insert(tag)
+}
