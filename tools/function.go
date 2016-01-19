d3124205aae30c95a7922448d989dd62bd7976c1
lixuancn <lixuan868686@163.com>
2016-01-19 18:30:06 +0800

    test

--- tools/function.go
+++ tools/function.go
@@ -16,6 +16,8 @@ import (
 	"io/ioutil"
 	"encoding/json"
 	"bytes"
+	"math"
+	"net/http"
 )
 
 //创建GUID
@@ -116,7 +118,16 @@ func ReadFileString(filename string)string{
 	return string(content)
 }
 
+//字符串写入文件
+func WriteFile(filename, data string) error {
+	str := []byte(data)
+	return ioutil.WriteFile(filename, str, 0644)
+}
 
+//创建目录
+func MkDir(path string) error {
+	return os.Mkdir(path, os.ModePerm)
+}
 
 //JsonEncode
 func JsonEncode(data interface{})(string, error){
@@ -127,9 +138,13 @@ func JsonEncode(data interface{})(string, error){
 //JsonDecode
 func JsonDecode(data string)(interface{}, error){
 	dataByte := []byte(data)
-	var j interface{}
-	err := json.Unmarshal(dataByte, &j)
-	return dataByte, err
+	type Json struct {
+		Name string
+		Age int
+	}
+	var jsons []Json
+	err := json.Unmarshal(dataByte, &jsons)
+	return jsons, err
 }
 
 //分割字符串
@@ -163,4 +178,51 @@ func TagSplit(keywords []byte)string{
 		content = content + fmt.Sprintf(`<a class ="tags" href="/tag/%s/1">%s</a>`, value, value)
 	}
 	return content
+}
+
+//四舍五入
+func Round(val float64, places int) float64{
+	var t float64
+	f := math.Pow10(places)
+	x := val * f
+	if math.IsInf(x, 0) || math.IsNaN(x) {
+		return val
+	}
+	if x >= 0.0 {
+		t = math.Ceil(x)
+		if (t-x) > 0.50000000001 {
+			t -= 1.0
+		}
+	}else{
+		t = math.Ceil(-x)
+		if (t+x) > 0.50000000001 {
+			t -= 1.0
+		}
+		t = -t
+	}
+	x = t / f
+	if !math.IsInf(x, 0){
+		return x
+	}
+	return t
+}
+
+func Get(reqUrl string) (string, error){
+	response, err := http.Get(reqUrl)
+	if err != nil {
+		return "", err
+	}
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		response.Body.Close()
+		return "", err
+	}
+	return string(body), nil
+}
+
+func Strim(str string) string {
+	str = strings.Replace(str, "\t", "", -1)
+	str = strings.Replace(str, "\n", "", -1)
+	str = strings.Replace(str, "\r", "", -1)
+	str = strings.Replace(str, " ", "", -1)
 }
--- tools/function_test.go
+++ tools/function_test.go
@@ -17,27 +17,51 @@ func Test_RandString(t *testing.T){
 	fmt.Println("Test_RandString" + ": " + str)
 }
 
-func Test_JsonEncode(t *testing.T){
-	arr := [3]string{"a", "b", "c"}
+func Test_Md5(t *testing.T){
+	fmt.Println("Test_Md5: " + tools.Md5("123456"))
+}
+func Test_Json(t *testing.T){
+	arr := [3]map[string]interface{}{{"name":"lixuan1", "age":25}, {"name":"lixuan2", "age":26}, {"name":"lixuan3", "age":27}}
+	//encode
 	json, err := tools.JsonEncode(arr)
 	if err != nil {
 		fmt.Println("Test_JsonEncode error" + ": ", err)
+	}else{
+		fmt.Println("Test_JsonEncode" + ": " + json)
 	}
-	fmt.Println("Test_JsonEncode" + ": " + json)
-}
-
-func Test_JsonDecode(t *testing.T){
-	json := `[{"name":"lixuan1", "age":25}, {"name":"lixuan2", "age":26}, {"name":"lixuan3", "age":27}]`
-	arr, err := tools.JsonDecode(json)
+	//decode
+	data, err := tools.JsonDecode(json)
 	if err != nil {
 		fmt.Println("Test_JsonDecode error" + ": ", err)
+	}else{
+		fmt.Println("Test_JsonDecode" + ": ", data)
 	}
-	fmt.Println("Test_JsonDecode" + ": ", arr)
 }
 
 func Test_TagSplit(t *testing.T){
 	var keywords = []byte("tag1, tag2, tag3")
 	tags := tools.TagSplit(keywords)
 	fmt.Println("Test_TagSplit" + ": " + tags)
+}
+
+func Test_WriteFile(t *testing.T){
+	path := "/tmp/go/"
+	if !tools.PathExist(path) {
+		err := tools.MkDir(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	filename := path + "Test_WriteFile.txt"
+	data := `hello go` + "\n"
+	err := tools.WriteFile(filename, data)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func Test_Get(t *testing.T){
+	content, _ := tools.Get("http://www.lanecn.com")
+	fmt.Println("Test_Get" + ": " + content)
 }
 
